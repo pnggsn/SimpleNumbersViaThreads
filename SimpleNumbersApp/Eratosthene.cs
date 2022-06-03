a8de2ee7e12c86122d457eded3b4586e58f24c83
Mosunova <81022626+pnggsn@users.noreply.github.com>
2022-06-03 11:22:01 +0700

    ver 3.fin

---
+++ SimpleNumbersApp/Eratosthene.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleNumbersApp
+{
+    class Eratosthene
+    {
+        //последовательное решение поиска
+        private static int TrueCount = 0;
+        private static bool solution(List<int> a)
+        {
+            int trueCount = 0;
+            foreach (int i in a)
+            {
+                int def = (int)Math.Sqrt(i);
+                int del = 0;
+                for (int j = 1; j <= def; j++)
+                {
+                    if (i % j == 0)
+                        del++;
+                }
+                if (del == 0 || i == 1 || del == 1)
+                    trueCount++;
+            }
+            TrueCount += trueCount;
+
+            if (TrueCount == 0)
+                return false;
+            else
+                return true;
+        }
+
+        public static void Execute(List<int> a)
+        {
+            Console.WriteLine(solution(a));
+        }
+    }
+}
